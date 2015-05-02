687bfffbbdf3f03cd063efba2e5f70677c2b9322
vaishnavi5054 <vaishnavi.aienampudi93@gmail.com>
2015-05-01 21:55:39 -0500

    ASE Increment 4
    Calorie2Grocery

---
+++ ASE_Increment_3 PG_12/ImportRecipe/ImportRecipe/app/src/main/java/mpg12/umkc/ase/importrecipe/MainActivity.java
@@ -0,0 +1,51 @@
+package mpg12.umkc.ase.importrecipe;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView webview = (WebView) findViewById(R.id.webView);
+
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setVerticalScrollBarEnabled(true);
+
+        webview.setScrollbarFadingEnabled(false);
+        webview.loadUrl("file:///android_asset/ImportRecipe.html");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
